57a1fa84b8e8883dcf78584f45d5819e720f253f
Tomas Martinez <tomashmartinez@javeriana.edu.co>
2023-05-25 20:20:49 -0500

    Exito

--- src/main/java/com/example/demo/LibroController.java
+++ src/main/java/com/example/demo/LibroController.java
@@ -1,27 +1,25 @@
 package com.example.demo;
 
-import java.util.*;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
-
 @RequestMapping("/libro")
 public class LibroController {
     @Autowired
-    LibroService service;
+    private LibroService libroService;
 
     @GetMapping("/listar")
     public List<libro>listar(){
-        return service.Listar();
+        return libroService.Listar();
     }
 
     @PostMapping ("/agregar")
     public libro agregar(@RequestBody libro p){
-        return service.add(p);
+        return libroService.add(p);
     }
 }
--- src/main/java/com/example/demo/LibroService.java
+++ src/main/java/com/example/demo/LibroService.java
@@ -2,7 +2,9 @@ package com.example.demo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LibroService { 
     @Autowired
     LibroRepository repository;
